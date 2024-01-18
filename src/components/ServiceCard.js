87cfcf794174554b7550d1b276e6a8a6c0e3f636
kaungpyaesoneaunggic <kaungpyaesoneaung@gicjp.com>
2024-01-18 22:52:52 +0630

    first

---
+++ src/components/ServiceCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ServiceCard() {
+  return (
+    <div>
+      <button className='' style={{display:'inline-block',border:'3px solid', borderRadius:'10px',margin:'0', padding:'10px 20px 10px 20px'}}>{text}</button>
+    </div>
+  )
+}
